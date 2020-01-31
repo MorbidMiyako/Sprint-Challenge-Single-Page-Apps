89a0ee42dd770e2efefa2bb174a66c65faa3b804
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-01-31 17:33:41 +0100

    finished a way to display API info, a button to home page, a button from home page to character list, and an unlinked button for more info on each character

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = (props) => {
+  console.log(props, "CharacterCard props")
+  return (
+    <>
+      <h1>{props.character.name}</h1>
+      <h3>Status: {props.character.status}</h3>
+      <p>Species: {props.character.species}</p>
+      <img src={props.character.image} alt={""} />
+      <div>
+        <Link to={props.character.url}>
+          <button>
+            More info about {props.character.name}
+          </button>
+        </Link>
+      </div>
+    </>
+  )
 }
+
+export default CharacterCard
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res, "axios full response")
+        console.log(res.data.results, "throwing into setCharacters")
+        setCharacters(res.data.results)
+      })
+      .catch(err => console.log(err))
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return (
+          <CharacterCard character={character} key={character.id} />
+        )
+      })}
     </section>
   );
 }
